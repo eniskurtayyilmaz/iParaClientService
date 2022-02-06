753eecfcefa2e78da25a455a6895e18aef53f0f8
eniskurtayyilmaz <kurtayyilmaz@gmail.com>
2022-02-06 22:14:19 +0300

    #51 için geliştirme tamamlandı.
    Ancak test caselerdeki  #53 ve #54 akışlarında hatalar tespit edildi.

--- iParaClientService.Integration.Tests/iParaLinkPaymentCreateAdapterTests.cs
+++ iParaClientService.Integration.Tests/iParaLinkPaymentCreateAdapterTests.cs
@@ -6,6 +6,7 @@ using iParaClientService.Adapter;
 using iParaClientService.Model;
 using iParaClientService.Model.Request;
 using iParaClientService.Service;
+using iParaClientService.Utils;
 
 namespace iParaClientService.Integration.Tests
 {
@@ -62,6 +63,12 @@ namespace iParaClientService.Integration.Tests
                 var result = adapter.Execute(model);
 
                 Trace.WriteLine(result.ErrorMessage);
+                Trace.WriteLine(result.ErrorCode);
+
+                if (!result.IsValid)
+                {
+                    Trace.WriteLine(JsonBuilderHelpers.SerializeToJsonString(model));
+                }
 
                 Assert.IsTrue(result.IsValid);
                 Assert.IsNull(result.ErrorCode);
@@ -109,6 +116,12 @@ namespace iParaClientService.Integration.Tests
                 var result = adapter.Execute(model);
 
                 Trace.WriteLine(result.ErrorMessage);
+                Trace.WriteLine(result.ErrorCode);
+
+                if (!result.IsValid)
+                {
+                    Trace.WriteLine(JsonBuilderHelpers.SerializeToJsonString(model));
+                }
 
                 Assert.IsTrue(result.IsValid);
                 Assert.IsNull(result.ErrorCode);
@@ -159,6 +172,12 @@ namespace iParaClientService.Integration.Tests
                 var result = adapter.Execute(model);
 
                 Trace.WriteLine(result.ErrorMessage);
+                Trace.WriteLine(result.ErrorCode);
+
+                if (!result.IsValid)
+                {
+                    Trace.WriteLine(JsonBuilderHelpers.SerializeToJsonString(model));
+                }
 
                 Assert.IsTrue(result.IsValid);
                 Assert.IsNull(result.ErrorCode);
@@ -171,6 +190,199 @@ namespace iParaClientService.Integration.Tests
                 Assert.IsNotNull(result.ResponseMessage);
             }
         }
+
+
+        [TestMethod]
+
+        public void Can_Create_Link_Payment_Valid_When_InstallmentList_All_Options()
+        {
+            string baseUrl = "https://api.ipara.com/";
+            iParaConnectionMode mode = iParaConnectionMode.Test;
+            string version = "1.0";
+
+            using (var connection = new iParaClientConnection(baseUrl, publicKey, privateKey, mode, version))
+            {
+                var model = new iParaLinkPaymentCreateRequest
+                {
+                    ThreeD = false,
+                    ClientIp = "127.0.0.1",
+                    CommissionType = CommissionType.Dealer,
+                    Email = "kurtayyilmaz@gmail.com",
+                    SendEmail = true,
+                    Name = "Kurtay",
+                    Surname = "Yılmaz",
+                    TcCertificate = "18946604794",
+                    TaxNumber = "",
+                    Echo = "",
+                    Mode = "T",
+                    ExpireDate = DateTime.Now.AddDays(1).ToString("yyyy-MM-dd hh:mm:ss"),
+                    Gsm = "5397143516",
+                    InstallmentList = new List<int>()
+                    {
+                        2,3,4,5,6,7,8,9,10,11,12
+                    },
+                    Description = "Taksitli satış"
+                };
+                model.SetAmount(10);
+
+                var adapter = new iParaLinkPaymentCreateAdapter(connection);
+                var result = adapter.Execute(model);
+
+                Trace.WriteLine(result.ErrorMessage);
+                Trace.WriteLine(result.ErrorCode);
+
+                if (!result.IsValid)
+                {
+                    Trace.WriteLine(JsonBuilderHelpers.SerializeToJsonString(model));
+                }
+
+                Assert.IsTrue(result.IsValid);
+                Assert.IsNull(result.ErrorCode);
+                Assert.IsNull(result.ErrorMessage);
+
+                Assert.AreEqual(model.Amount, result.Amount.Value);
+                Assert.IsNotNull(result.Link);
+                Assert.IsNotNull(result.LinkPaymentId);
+                Assert.IsNotNull(result.Result);
+                Assert.IsNotNull(result.ResponseMessage);
+            }
+        }
+
+
+
+        [TestMethod]
+
+        public void Can_Create_Link_Payment_Valid_When_InstallmentList_2_3_5_Options_And_CommissionTypes_Dealer()
+        {
+            string baseUrl = "https://api.ipara.com/";
+            iParaConnectionMode mode = iParaConnectionMode.Test;
+            string version = "1.0";
+
+            using (var connection = new iParaClientConnection(baseUrl, publicKey, privateKey, mode, version))
+            {
+                var model = new iParaLinkPaymentCreateRequest
+                {
+                    ThreeD = false,
+                    ClientIp = "127.0.0.1",
+                    CommissionType = CommissionType.Seller,
+                    Email = "kurtayyilmaz@gmail.com",
+                    SendEmail = true,
+                    Name = "Kurtay",
+                    Surname = "Yılmaz",
+                    TcCertificate = "18946604794",
+                    TaxNumber = "",
+                    Echo = "",
+                    Mode = "T",
+                    ExpireDate = DateTime.Now.AddDays(1).ToString("yyyy-MM-dd hh:mm:ss"),
+                    Gsm = "5397143516",
+                    InstallmentList = new List<int>()
+                    {
+                        2,3,4,5
+                    },
+                    InstallmentTwoCommissionType = CommissionType.Seller,
+                    InstallmentThreeCommissionType = CommissionType.Dealer,
+                    InstallmentFiveCommissionType= CommissionType.Dealer,
+                    Description = "Taksitli satış"
+                };
+                model.SetAmount(10);
+
+                var adapter = new iParaLinkPaymentCreateAdapter(connection);
+                var result = adapter.Execute(model);
+
+                Trace.WriteLine(result.ErrorMessage);
+                Trace.WriteLine(result.ErrorCode);
+
+                if (!result.IsValid)
+                {
+                    Trace.WriteLine(JsonBuilderHelpers.SerializeToJsonString(model));
+                }
+
+                Assert.IsTrue(result.IsValid);
+                Assert.IsNull(result.ErrorCode);
+                Assert.IsNull(result.ErrorMessage);
+
+                Assert.AreEqual(model.Amount, result.Amount.Value);
+                Assert.IsNotNull(result.Link);
+
+                Trace.WriteLine(result.Link);
+
+                Assert.IsNotNull(result.LinkPaymentId);
+                Assert.IsNotNull(result.Result);
+                Assert.IsNotNull(result.ResponseMessage);
+            }
+        }
+
+        [TestMethod]
+
+        public void Can_Create_Link_Payment_Valid_When_InstallmentList_All_Options_And_CommissionTypes_Dealer()
+        {
+            string baseUrl = "https://api.ipara.com/";
+            iParaConnectionMode mode = iParaConnectionMode.Test;
+            string version = "1.0";
+
+            using (var connection = new iParaClientConnection(baseUrl, publicKey, privateKey, mode, version))
+            {
+                var model = new iParaLinkPaymentCreateRequest
+                {
+                    ThreeD = false,
+                    ClientIp = "127.0.0.1",
+                    CommissionType = CommissionType.Dealer,
+                    Email = "kurtayyilmaz@gmail.com",
+                    SendEmail = true,
+                    Name = "Kurtay",
+                    Surname = "Yılmaz",
+                    TcCertificate = "18946604794",
+                    TaxNumber = "",
+                    Echo = "",
+                    Mode = "T",
+                    ExpireDate = DateTime.Now.AddDays(1)
+                        .ToString("yyyy-MM-dd hh:mm:ss"),
+                    Gsm = "5397143516",
+                    InstallmentList = new List<int>()
+                    {
+                        2,
+                        3,
+                        4,
+                        5,
+                        6,
+                        7,
+                        8,
+                        9,
+                        10,
+                        11,
+                        12
+                    },
+                    InstallmentTwoCommissionType = CommissionType.Dealer,
+                    InstallmentThreeCommissionType = CommissionType.Dealer,
+                    InstallmentFourCommissionType = CommissionType.Dealer,
+                    Description = "Taksitli satış",
+
+                };
+                model.SetAmount(10);
+
+                var adapter = new iParaLinkPaymentCreateAdapter(connection);
+                var result = adapter.Execute(model);
+
+                Trace.WriteLine(result.ErrorMessage);
+                Trace.WriteLine(result.ErrorCode);
+
+                if (!result.IsValid)
+                {
+                    Trace.WriteLine(JsonBuilderHelpers.SerializeToJsonString(model));
+                }
+
+                Assert.IsTrue(result.IsValid);
+                Assert.IsNull(result.ErrorCode);
+                Assert.IsNull(result.ErrorMessage);
+
+                Assert.AreEqual(model.Amount, result.Amount.Value);
+                Assert.IsNotNull(result.Link);
+                Assert.IsNotNull(result.LinkPaymentId);
+                Assert.IsNotNull(result.Result);
+                Assert.IsNotNull(result.ResponseMessage);
+            }
+        }
+
 
         [TestMethod]
 
@@ -204,7 +416,12 @@ namespace iParaClientService.Integration.Tests
                 var result = adapter.Execute(model);
 
                 Trace.WriteLine(result.ErrorMessage);
+                Trace.WriteLine(result.ErrorCode);
 
+                if (!result.IsValid)
+                {
+                    Trace.WriteLine(JsonBuilderHelpers.SerializeToJsonString(model));
+                }
                 Assert.IsFalse(result.IsValid);
                 Assert.IsNotNull(result.ErrorCode);
                 Assert.IsNotNull(result.ErrorMessage);
