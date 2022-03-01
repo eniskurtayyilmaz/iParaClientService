7daddd490d44e2c9664c5fe9b5490fa07dd1f613
Enis Kurtay Yılmaz <kurtayyilmaz@gmail.com>
2022-03-01 21:33:58 +0300

    Description eklendi

--- iParaClientService/Model/Request/LinkState.cs
+++ iParaClientService/Model/Request/LinkState.cs
@@ -1,28 +1,36 @@
-﻿namespace iParaClientService.Model.Request
+﻿using System.ComponentModel;
+
+namespace iParaClientService.Model.Request
 {
 
     public enum LinkState
     {
+        [Description("Link İsteği Yapıldı")]
         /// <summary>
         /// Link isteği alındı
         /// </summary>
         LinkRequest = 0,
+        [Description("Link Oluşturuldu")]
         /// <summary>
         /// Link URL’i yaratıldı
         /// </summary>
         LinkCreated = 1,
+        [Description("Link Gönderildi")]
         /// <summary>
         /// Link Gönderildi, Ödeme Bekleniyor
         /// </summary>
         LinkSended = 2,
+        [Description("Ödeme Başarılı")]
         /// <summary>
         /// Link ile Ödeme Başarılı
         /// </summary>
         LinkPaymentDone = 3,
+        [Description("Zaman Aşımına Uğradı")]
         /// <summary>
         /// Link Zaman Aşımına Uğradı
         /// </summary>
         LinkPaymentTimeout = 98,
+        [Description("Link Silindi")]
         /// <summary>
         /// Link Silindi
         /// </summary>
